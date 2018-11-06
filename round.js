ecd57e6debf5b0dad5194bf7075dbffa089a0712
Jimmy Svensson <jimmy888swe@gmail.com>
2018-11-06 14:43:39 +0100

    forsta commit

---
+++ round.js
@@ -0,0 +1,7 @@
+var tal;
+var spelare = ['Colorado Avalanche','Boston Bruins','Tampa Bay Lightning',
+'Calgary Flames','Washington Capitals','San Jose Sharks'];
+
+for (let i = 1; i < spelare.length; i++){
+
+}
